537e2f93c188392b4dd5c2cb2b8eb61161e06aa6
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2016-05-24 11:35:00 +0200

    sample test added

---
+++ test/references.ts
@@ -0,0 +1,2 @@
+/// <reference path="../vendor/jspm_packages/npm/typemoq@0.0.6/typemoq.node.d.ts" />
+/// <reference path="../vendor/typings/index.d.ts" />
---
+++ test/spec/sample.ts
@@ -0,0 +1,15 @@
+/// <reference path="../references.ts" />
+
+import {TypeMoq} from 'typemoq';
+import * as Test from '../../src/index';
+import {APIWrapper} from '../../src/SCORM/APIWrapper';
+
+describe('jhgjgj', () => {
+
+  it('m,jh,jhg,', () => {
+    console.debug('3: typemoq =>', TypeMoq.Mock);
+    console.debug('10: Test =>', Test);
+    console.debug('12: APIWrapper =>', APIWrapper);
+  });
+
+});
